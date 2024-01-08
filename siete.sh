9e264c1b0b16bd708d464355fe088a35a07113a7
erikpr04 <erikpr32@gmail.com>
2024-01-08 11:49:30 +0100

    afegir sh

---
+++ siete.sh
@@ -0,0 +1,22 @@
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
+read -p "PALABRA" pa
+echo "$pa"
